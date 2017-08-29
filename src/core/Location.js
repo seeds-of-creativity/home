fd7329fc8ca6a5df2bc426577389ead83ed1adf4
iraleigh <iain.raleigh.dev@gmail.com>
2017-08-28 17:37:41 -0700

    Fixed linting errors in src/core

--- src/core/Location.js
+++ src/core/Location.js
@@ -11,6 +11,8 @@ import createHistory from 'history/lib/createBrowserHistory';
 import createMemoryHistory from 'history/lib/createMemoryHistory';
 import useQueries from 'history/lib/useQueries';
 
-const location = useQueries(process.env.BROWSER ? createHistory : createMemoryHistory)();
+const location = useQueries(
+  process.env.BROWSER ? createHistory : createMemoryHistory,
+)();
 
 export default location;
